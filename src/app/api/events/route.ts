ee4c42570dd2399fa5786b281819c029049c61f6
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-02-03 17:15:33 +0600

    Events get api created

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,27 @@
+import { mongoConnect } from "@/lib/mongoConnect";
+import { NextResponse } from "next/server";
+
+async function GET() {
+  try {
+    const { db, client } = await mongoConnect();
+    const events = await db.collection("events").find().toArray();
+
+    const formattedEvents = events.map((event) => ({
+      id: event._id.toString(),
+      title: event.title,
+      date: event.date,
+      location: event.location,
+      image: event.image,
+      description: event.description,
+    }));
+    //   client.close(); korte hobe Response pathanor age jodi dbConnect file ta use kori
+    return NextResponse.json(formattedEvents);
+
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+        {error: 'Failed to fetch events'},
+        {status: 500}
+    )
+  }
+}
