6be11e067b5425153b5bc856c3aeea6d0534515b
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-02-08 12:03:41 +0600

    Fixed shadcn breadcrumb error problem by install

---
+++ src/app/(dashboardLayout)/dashboard/overview/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div>
+            Overview page
+        </div>
+    );
+};
+
+export default Overview;
--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/components/designs/AppSidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
