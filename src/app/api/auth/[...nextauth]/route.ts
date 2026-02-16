21c0a33c61380fe29a77dc4d0bd67d5284310764
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-02-16 22:03:31 +0600

    Created authOptions file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,7 @@
+import { authOptions } from "@/authOptions/authOptions"
 import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error("Missing Google OAuth credentials");
-}
 
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-   GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    // ...add more providers here
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-}
 const handler = NextAuth(authOptions)
 export {handler as GET, handler as POST}
 
---
+++ src/authOptions/authOptions.ts
@@ -0,0 +1,18 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error("Missing Google OAuth credentials");
+}
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+   GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+}
